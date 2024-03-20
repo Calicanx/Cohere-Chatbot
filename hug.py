142766f6cba6b86117e774a45631cf37887a8faa
0xSc0rpi0n <55562628+Calicanx@users.noreply.github.com>
2024-03-20 15:43:22 +0300

    FastAPI version of the chatbot

--- hug.py
+++ hug.py
@@ -2,19 +2,30 @@ from dotenv import load_dotenv
 from langchain_community.chat_models import ChatCohere
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
 
 load_dotenv(".env")
 
 llm = ChatCohere()
 
-prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are world class technical documentation writer."),
-    ("user", "{input}")
-])
 
-output_parser = StrOutputParser()
+@app.get("/input")
+def access_chatbot(question):
 
-chain = prompt | llm | output_parser
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are world class technical documentation writer."),
+        ("user", "{input}")
+    ])
 
-result = chain.invoke({"input": "Can you please tell me about Pavlov's experiment?"})
-print(result)
+    output_parser = StrOutputParser()
+
+    chain = prompt | llm | output_parser
+
+    result = chain.invoke({"input":question})
+    return result
+
+if __name__ == '__main__':
+    uvicorn(app, host = '127.0.0.1', port = 8000)
